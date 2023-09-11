1be190649e22150d9315e618cc9aa041f5cba947
tobexint <tobexint@gmail.com>
2023-09-11 04:22:14 +0000

    Update info

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
 from flask import Flask, request, jsonify
-from datetime import datetime
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
-def get_info():
+def info():
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
+
+    current_day = datetime.datetime.now().strftime('%A')
+    utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
     github_file_url = 'https://github.com/Tobexint/Zuri/blob/main/app.py'
     github_repo_url = 'https://github.com/tobexint/Zuri'
 
-    utc_time = datetime.utcnow()
-    if utc_time.hour < 22 and utc_time.hour > 2:
-        return jsonify({
+    response = {
             'slack_name': slack_name,
-            'current_day': "Monday",
-            'utc_time': str(utc_time),
+            'current_day': current_day,
+            'utc_time': utc_time,
             'track': track,
-            'status': 'Error',
-            'message': 'The current UTC time is not within +/- 2 hours.'
-            }), 400
-    else:
-        day_of_week = datetime.utcnow().strftime('%A')
-        return jsonify({
-            'slack_name': slack_name,
-            'current_day': "Monday",
-            'utc_time': str(utc_time),
-            'track': track,
-            'github_url_file': github_file_url,
+            'github_file_url': github_file_url,
             'github_repo_url': github_repo_url,
-            'status_code': 200,
-            }), 200
+            'status_code': 200
+            }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
